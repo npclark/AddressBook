6cbef0d6ea7a3cfc1fc628b2d83a9b6d0351c6ee
Noah <noah.clark500@gmail.com>
2022-10-28 18:55:27 -0500

    Added Contact class

--- AddressBook/src/model/Contact.java
+++ AddressBook/src/model/Contact.java
@@ -6,6 +6,16 @@
 
 package model;
 
-public class Contact {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+public class Contact {
+	
+	
+	private int id; 			// unique identifier for each Contact
+	private String phoneNum;	// Contact's phone number
+	private String name;		// Contact's name
+	private State state;		// State that contact lives in
 }
--- AddressBook/src/module-info.java
+++ AddressBook/src/module-info.java
@@ -1,2 +1,3 @@
 module AddressBook {
+	requires javax.persistence;
 }
