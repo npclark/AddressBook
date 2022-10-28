1bf184ca520236f636bc9011d488b7e7a53db8b6
Noah <noah.clark500@gmail.com>
2022-10-28 18:56:28 -0500

    Update Contact.java

--- AddressBook/src/model/Contact.java
+++ AddressBook/src/model/Contact.java
@@ -7,15 +7,54 @@
 package model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
+@Entity
+@Table(name="contact")
 public class Contact {
-	
-	
+	@Id
+	@GeneratedValue
 	private int id; 			// unique identifier for each Contact
 	private String phoneNum;	// Contact's phone number
 	private String name;		// Contact's name
 	private State state;		// State that contact lives in
+	
+	public Contact() {
+		super();
+	}
+	public Contact(String phoneNum, String name, State state) {
+		this.phoneNum = phoneNum;
+		this.name = name;
+		this.state = state;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	@Override
+	public String toString() {
+		return "Contact [id=" + id + ", phoneNum=" + phoneNum + ", name=" + name + "]";
+	}
 }
